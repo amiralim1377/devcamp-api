b66649d1c1ecd6c03158d60c6f7782a5de54d097
AMIRALI <alimoradi7718@gmail.com>
2026-07-29 16:15:39 +0330

    feat: add user model with schema and interfaces for MongoDB

---
+++ src/models/user.model.ts
@@ -0,0 +1,40 @@
+import { model, Schema, Types } from "mongoose";
+
+export interface IUser extends Document {
+  _id: Types.ObjectId;
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  role: "student" | "instructor" | "admin";
+}
+
+export interface IUserMethods extends Document {}
+
+const userSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    required: [true, "'Please tell us your name!'"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email!"],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  role: {
+    type: String,
+    enum: ["student", "instructor", "admin"],
+    default: "student",
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password!"],
+    select: false,
+  },
+});
+
+const User = model<IUser>("User", userSchema);
+export default User;
