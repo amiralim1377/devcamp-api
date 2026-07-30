85984d02cd9a11196c20b212fc863eff061164c4
AMIRALI <alimoradi7718@gmail.com>
2026-07-30 18:55:12 +0330

    feat: implement login functionality with password verification

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -9,6 +9,19 @@ class AuthController {
 
     createSendToken(newUser, 201, req, res);
   }
+
+  async login(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select("+password");
+
+    if (
+      !user ||
+      !(await user.correctPassword(password, user.password as string))
+    ) {
+      return next(new AppError("Incorrect email or password", 401));
+    }
+    createSendToken(user, 200, req, res);
+  }
 }
 
 export default new AuthController();
