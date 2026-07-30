3983556c92577ba0c832623d69e50e31b499dea3
AMIRALI <alimoradi7718@gmail.com>
2026-07-30 18:55:23 +0330

    feat: add login route with validation to user routes

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import authController from "../controllers/authController.js";
 import { validateRequest } from "../middlewares/validateRequest.js";
-import { signupSchema } from "../schemas/auth.schema.js";
+import { loginSchema, signupSchema } from "../schemas/auth.schema.js";
 const router = express.Router();
 
 router.post("/signup", validateRequest(signupSchema), authController.signup);
+router.post("/login", validateRequest(loginSchema), authController.login);
 
 export default router;
