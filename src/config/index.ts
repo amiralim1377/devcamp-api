53eb8b970ee4268d3ea3168e5af42418734134cc
AMIRALI <alimoradi7718@gmail.com>
2026-07-30 10:54:26 +0330

    fix: remove mongoUri from config and ensure local database variable is used

--- src/config/index.ts
+++ src/config/index.ts
@@ -16,10 +16,10 @@ const getEnvVar = (key: string, defaultValue?: string): string => {
 const config = {
   port: parseInt(getEnvVar("PORT", "3000")),
   nodeEnv: getEnvVar("NODE_ENV", "development"),
-  mongoUri: getEnvVar("MONGO_URI"),
   jwtSecret: getEnvVar("JWT_SECRET"),
   jwtExpiresIn: getEnvVar("JWT_EXPIRES_IN"),
   jwtCookieExpiresIn: getEnvVar("JWT_COOKIE_EXPIRES_IN"),
+  databaseLocal: getEnvVar("DATABASE_LOCAL"),
 };
 
 export { config };
