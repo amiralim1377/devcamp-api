7f2902ad4737a1cb6e9b9e1a4a2b345fa01333c8
AMIRALI <alimoradi7718@gmail.com>
2026-07-31 14:15:31 +0330

    feat: add logout method to AuthController for user session termination

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -83,6 +83,13 @@ class AuthController {
       next();
     };
   }
+  logout(req: Request, res: Response, next: NextFunction) {
+    res.cookie("jwt", "loggedout", {
+      expires: new Date(Date.now() + 10 * 1000),
+      httpOnly: true,
+    });
+    res.status(200).json({ status: "success" });
+  }
 }
 
 export default new AuthController();
