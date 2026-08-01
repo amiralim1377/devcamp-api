a967117daac5660229e15e15e0e8ae4dd41d7470
AMIRALI <alimoradi7718@gmail.com>
2026-08-01 12:14:10 +0330

    feat: enhance createSendToken function to include refresh token generation and session creation

--- src/utils/createSendToken.ts
+++ src/utils/createSendToken.ts
@@ -2,29 +2,68 @@ import { Response, Request, CookieOptions } from "express";
 import { config } from "../config/index.js";
 import { IUser } from "../models/user.model.js";
 import { signToken } from "./signToken.js";
+import crypto from "crypto";
+import { Session } from "../models/session.model.js";
 
 // user, statusCode, req, res
-export const createSendToken = (
+export const createSendToken = async (
   user: IUser,
   statusCode: number,
   req: Request,
   res: Response,
 ) => {
-  const token = signToken(user._id.toString());
+  // 1) Create Access Token (JWT)
+  const accessToken = signToken(user._id.toString());
 
+  // 2) Create Refresh Token (Opaque Token)
+  const refreshToken = crypto.randomBytes(64).toString("hex");
+
+  // 3) Hash Refresh Token
+  const refreshTokenHash = crypto
+    .createHash("sha256")
+    .update(refreshToken)
+    .digest("hex");
+
+  // Expiration Dates
+  const refreshTokenExpiresAt = new Date(
+    Date.now() + Number(config.jwtRefreshExpiresIn) * 24 * 60 * 60 * 1000,
+  );
+  const accessTokenExpiresAt = new Date(
+    Date.now() + Number(config.jwtExpiresIn) * 60 * 1000,
+  );
+
+  //  4) Create Session in Database
+  await Session.create({
+    user: user._id,
+    tokenHash: refreshTokenHash,
+    ipAddress: req.ip,
+    userAgent: req.headers["user-agent"],
+    expiresAt: refreshTokenExpiresAt,
+  });
+
+  // 5) Cookie Configuration
   const cookieOptions: CookieOptions = {
-    expires: new Date(
-      Date.now() + Number(config.jwtCookieExpiresIn) * 24 * 60 * 60 * 1000,
-    ),
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    secure: config.nodeEnv === "production",
+    sameSite: config.nodeEnv === "production" ? "none" : "lax",
   };
+  // Send Access Token Cookie
+  res.cookie("access_token", accessToken, {
+    ...cookieOptions,
 
-  res.cookie("jwt", token, cookieOptions);
+    expires: accessTokenExpiresAt,
+  });
+  // Send Refresh Token Cookie
+  res.cookie("refresh_token", refreshToken, {
+    ...cookieOptions,
 
+    expires: refreshTokenExpiresAt,
+  });
+
+  // response
   res.status(statusCode).json({
     status: "success",
+
     data: {
       user,
     },
