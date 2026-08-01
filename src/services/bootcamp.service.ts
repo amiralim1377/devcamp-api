6718528f480fa90e9a173475976d6afcde825c6a
AMIRALI <alimoradi7718@gmail.com>
2026-08-01 17:13:31 +0330

    feat: implement getBootcamp and deleteBootcamp methods in BootcampService

--- src/services/bootcamp.service.ts
+++ src/services/bootcamp.service.ts
@@ -1,4 +1,5 @@
 import { Bootcamp, IBootcamp } from "../models/bootcamp.model.js";
+import { AppError } from "../utils/AppError.js";
 
 class BootcampService {
   async createBootcamp(bootcampData: Partial<IBootcamp>) {
@@ -10,6 +11,23 @@ class BootcampService {
     const bootcamps = await Bootcamp.find();
     return bootcamps;
   }
+
+  async getBootcamp(bootcampId: string) {
+    const bootcamp = await Bootcamp.findById(bootcampId);
+    if (!bootcamp) {
+      throw new AppError("Bootcamp not found", 404);
+    }
+    return bootcamp;
+  }
+  async deleteBootcamp(bootcampId: string) {
+    const bootcamp = await Bootcamp.findByIdAndDelete(bootcampId);
+    if (!bootcamp) {
+      throw new AppError("Bootcamp not found", 404);
+    }
+
+    return bootcamp;
+  }
+  async updateBootcamp(bootcampId: string, updateData: Partial<IBootcamp>) {}
 }
 
 export default new BootcampService();
