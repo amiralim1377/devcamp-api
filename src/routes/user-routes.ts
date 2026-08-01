3e0a293537e16b8f0830cd49fa05ec4cd547444c
AMIRALI <alimoradi7718@gmail.com>
2026-08-01 14:46:36 +0330

    refactor: remove restrictTo middleware from user profile route

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import userController from "../controllers/userController.js";
-import { protect, restrictTo } from "../middlewares/auth.middleware.js";
+import { protect } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 // Protect all routes after this middleware
 router.use(protect);
 
-router.get("/me", restrictTo("instructor"), userController.getMe);
+router.get("/me", userController.getMe);
 
 export default router;
