a5d124b2f61025ff407ba42bdca4efb74096f223
AMIRALI <alimoradi7718@gmail.com>
2026-08-01 16:18:45 +0330

    fix: update token expiration handling in signToken function

--- src/utils/signToken.ts
+++ src/utils/signToken.ts
@@ -2,8 +2,10 @@ import jwt from "jsonwebtoken";
 import { config } from "../config/index.js";
 
 const signToken = (id: string): string => {
+  const expiresInSeconds = Number(config.jwtExpiresIn) * 60;
+
   return jwt.sign({ id }, config.jwtSecret, {
-    expiresIn: config.jwtExpiresIn as any,
+    expiresIn: expiresInSeconds,
   });
 };
 
