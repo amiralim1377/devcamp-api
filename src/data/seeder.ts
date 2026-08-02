8e91391e9e503cf57c6ce6deb840e46034d9275b
AMIRALI <alimoradi7718@gmail.com>
2026-08-02 11:39:43 +0330

    feat: add bootcamp and course data to JSON files and update seeder for data import

--- src/data/seeder.ts
+++ src/data/seeder.ts
@@ -1,37 +1,44 @@
 import fs from "fs";
+import mongoose from "mongoose";
 import { Bootcamp } from "../models/bootcamp.model.js";
 import { Course } from "../models/course.model.js";
 
 const bootcamps = JSON.parse(
-  fs.readFileSync("./_data/bootcamps.json", "utf-8"),
+  fs.readFileSync("./src/data/bootcamps.json", "utf-8"),
 );
-const courses = JSON.parse(fs.readFileSync("./_data/courses.json", "utf-8"));
+const courses = JSON.parse(fs.readFileSync("./src/data/courses.json", "utf-8"));
 
 // تابع ایمپورت داده‌ها
 const importData = async () => {
   try {
+    await mongoose.connect(process.env.DATABASE_LOCAL as string);
+    console.log("🌱 Database Connected...");
+
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     console.log("✅ Data Imported...");
     process.exit();
   } catch (err) {
     console.error(err);
+    process.exit(1);
   }
 };
 
-// تابع پاک کردن داده‌ها
 const deleteData = async () => {
   try {
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("🌱 Database Connected...");
+
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     console.log("🗑️ Data Destroyed...");
     process.exit();
   } catch (err) {
     console.error(err);
+    process.exit(1);
   }
 };
 
-// گرفتن دستور از ترمینال
 if (process.argv[2] === "-i") {
   importData();
 } else if (process.argv[2] === "-d") {
