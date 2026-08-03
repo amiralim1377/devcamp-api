bbfc8897b1d9f7cb6b984a279aaed6ee22894b31
AMIRALI <alimoradi7718@gmail.com>
2026-08-03 15:52:53 +0330

    feat: refactor userController to improve user ID handling and add error handling for password updates

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Response, Request } from "express";
 import userService from "../services/user.service.js";
+import { AppError } from "../utils/AppError.js";
 
 class UserController {
   async getAllUsers(req: Request, res: Response, next: NextFunction) {
@@ -17,7 +18,7 @@ class UserController {
 
   async getSingleUser(req: Request, res: Response, next: NextFunction) {
     try {
-      const userId = req.user!._id.toString();
+      const userId = req.params.id as string;
 
       const user = await userService.getUser(userId);
       res.status(200).json({
@@ -37,6 +38,21 @@ class UserController {
       },
     });
   }
+  async deleteMe(req: Request, res: Response, next: NextFunction) {
+    try {
+      const userId = req.user!._id.toString();
+
+      console.log("userId", userId);
+
+      await userService.deleteMe(userId);
+      res.status(204).json({
+        status: "success",
+        data: null,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 
   async createUser(req: Request, res: Response, next: NextFunction) {
     try {
@@ -52,14 +68,35 @@ class UserController {
 
   async updateUser(req: Request, res: Response, next: NextFunction) {
     try {
-      const userId = req.user!._id.toString();
+      if (req.body.password || req.body.passwordConfirm) {
+        return next(
+          new AppError("ادمین نمی‌تواند رمز عبور کاربران را تغییر دهد.", 400),
+        );
+      }
 
+      const userId = req.params.id as string;
       const updatedUser = await userService.updateUser(userId, req.body);
+
       res.status(200).json({
         status: "success",
-        data: {
-          user: updatedUser,
-        },
+        data: { user: updatedUser },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async updateDetails(req: Request, res: Response, next: NextFunction) {
+    try {
+      if (req.body.password || req.body.passwordConfirm) {
+        return next(new AppError("این مسیر برای تغییر رمز عبور نیست.", 400));
+      }
+
+      const userId = req.user!._id.toString();
+      const updatedUser = await userService.updateUser(userId, req.body);
+
+      res.status(200).json({
+        status: "success",
+        data: { user: updatedUser },
       });
     } catch (error) {
       next(error);
@@ -68,7 +105,7 @@ class UserController {
 
   async deleteUser(req: Request, res: Response, next: NextFunction) {
     try {
-      const userId = req.user!._id.toString();
+      const userId = req.params.id as string;
 
       await userService.deleteUser(userId);
       res.status(204).json({
