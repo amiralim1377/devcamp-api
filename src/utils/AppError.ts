601c6335c8326309c026e3b03c34685db06c7225
AMIRALI <alimoradi7718@gmail.com>
2026-08-08 19:14:29 +0330

    refactor: update AppError class to use HttpCodes and AppCodes for improved error handling

--- src/utils/AppError.ts
+++ src/utils/AppError.ts
@@ -1,17 +1,50 @@
+import { HttpCodes } from "./HttpCodes.js";
+import { AppCodes } from "./AppCodes.js";
+import { CustomLogger } from "./logger.js";
+
 export class AppError extends Error {
-  statusCode: number;
-  isOperational: boolean;
-  status: string;
+  public status: string;
+  public isOperational: boolean;
 
-  constructor(message: string, statusCode: number) {
+  private constructor(
+    public statusCode: HttpCodes,
+    public appCode: AppCodes,
+    message: string,
+    public details?: Record<string, unknown>,
+  ) {
     super(message);
-
-    this.statusCode = statusCode;
+    this.name = this.constructor.name;
     this.status = statusCode.toString().startsWith("4") ? "fail" : "error";
-
     this.isOperational = true;
 
     Error.captureStackTrace(this, this.constructor);
   }
+
+  static throwError(
+    caller: string,
+    statusCode: HttpCodes,
+    appCode: AppCodes,
+    message: string,
+    details?: Record<string, unknown>,
+  ): never {
+    const error = new AppError(statusCode, appCode, message, details);
+
+    CustomLogger.error(caller, error.appCode, error.message, {
+      statusCode: error.statusCode,
+      status: error.status,
+      ...error.details,
+      stack: error.stack,
+    });
+
+    throw error;
+  }
+
+  static create(
+    statusCode: HttpCodes,
+    appCode: AppCodes,
+    message: string,
+    details?: Record<string, unknown>,
+  ): AppError {
+    return new AppError(statusCode, appCode, message, details);
+  }
 }
-// for Operational-Error
