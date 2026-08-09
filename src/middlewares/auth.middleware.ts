4e950b8d229d46d4a91f0c1ec070205cc61ac820
AMIRALI <alimoradi7718@gmail.com>
2026-08-09 11:23:25 +0330

    refactor: improve error handling in auth middleware using AppError, HttpCodes, and AppCodes

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -3,67 +3,71 @@ import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import { AppError } from "../utils/AppError.js";
 import { config } from "../config/index.js";
+import { HttpCodes } from "../utils/HttpCodes.js";
+import { AppCodes } from "../utils/AppCodes.js";
 
 export const protect = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  try {
-    let token;
+  let token;
 
-    if (req.cookies && req.cookies.access_token) {
-      token = req.cookies.access_token;
-    } else if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
-    ) {
-      token = req.headers.authorization.split(" ")[1];
-    }
+  if (req.cookies && req.cookies.access_token) {
+    token = req.cookies.access_token;
+  } else if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
 
-    if (!token) {
-      return next(
-        new AppError(
-          "You are not logged in! Please log in to get access.",
-          401,
-        ),
-      );
-    }
-
-    const decoded = jwt.verify(
-      token,
-      config.jwtSecret as string,
-    ) as jwt.JwtPayload;
-
-    const currentUser = await User.findById(decoded.id);
-
-    if (!currentUser) {
-      return next(
-        new AppError(
-          "The user belonging to this token does no longer exist.",
-          401,
-        ),
-      );
-    }
-
-    req.user = currentUser;
-    next();
-  } catch (error) {
-    return next(
-      new AppError("Invalid or expired token! Please log in again.", 401),
+  if (!token) {
+    AppError.throwError(
+      "protect Middleware",
+      HttpCodes.UNAUTHORIZED,
+      AppCodes.UNAUTHORIZED_ACCESS,
+      "You are not logged in! Please log in to get access.",
     );
   }
+
+  const decoded = jwt.verify(
+    token,
+    config.jwtSecret as string,
+  ) as jwt.JwtPayload;
+
+  const currentUser = await User.findById(decoded.id);
+
+  if (!currentUser) {
+    AppError.throwError(
+      "protect Middleware",
+      HttpCodes.UNAUTHORIZED,
+      AppCodes.UNAUTHORIZED_ACCESS,
+      "The user belonging to this token does no longer exist.",
+    );
+  }
+
+  req.user = currentUser;
+  next();
 };
 
 export const restrictTo = (...roles: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
     if (!req.user) {
-      return next(new AppError("You are not logged in.", 401));
+      AppError.throwError(
+        "restrictTo Middleware",
+        HttpCodes.UNAUTHORIZED,
+        AppCodes.UNAUTHORIZED_ACCESS,
+        "You are not logged in.",
+      );
     }
 
     if (!roles.includes(req.user.role)) {
-      return next(
-        new AppError("You do not have permission to perform this action.", 403),
+      AppError.throwError(
+        "restrictTo Middleware",
+        HttpCodes.FORBIDDEN,
+        AppCodes.FORBIDDEN_ACCESS,
+        "You do not have permission to perform this action.",
       );
     }
 
