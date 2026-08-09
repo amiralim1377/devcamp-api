0c284d55902605e285a62638f81dd6d263b0a576
AMIRALI <alimoradi7718@gmail.com>
2026-08-09 11:02:52 +0330

    refactor: standardize response handling in BootcampController using ApiResponse and HttpCodes

--- src/controllers/bootcampController.ts
+++ src/controllers/bootcampController.ts
@@ -1,121 +1,105 @@
-import { NextFunction, Response, Request } from "express";
+import { Response, Request } from "express";
 import bootcampService from "../services/bootcamp.service.js";
 import { AppError } from "../utils/AppError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { HttpCodes } from "../utils/HttpCodes.js";
+import { AppCodes } from "../utils/AppCodes.js";
 
 class BootcampController {
-  async getAllBootcamps(req: Request, res: Response, next: NextFunction) {
-    try {
-      const bootcamps = await bootcampService.getAllBootcamps(req.query);
-      res.status(200).json({
-        status: "success",
-        results: bootcamps.length,
-        data: {
-          bootcamps,
-        },
-      });
-    } catch (error) {
-      next(error);
-    }
+  async getAllBootcamps(req: Request, res: Response) {
+    const bootcamps = await bootcampService.getAllBootcamps(req.query);
+
+    ApiResponse.send(
+      res,
+      HttpCodes.OK,
+      "Bootcamps retrieved successfully.",
+      { bootcamps },
+      { results: bootcamps.length },
+    );
   }
 
-  async getSingleBootcamp(req: Request, res: Response, next: NextFunction) {
-    try {
-      const bootcampId = req.params.id as string;
-      const bootcamp = await bootcampService.getBootcamp(bootcampId);
+  async getSingleBootcamp(req: Request, res: Response) {
+    const bootcampId = req.params.id as string;
+    const bootcamp = await bootcampService.getBootcamp(bootcampId);
 
-      res.status(200).json({
-        status: "success",
-        data: {
-          bootcamp,
-        },
-      });
-    } catch (error) {
-      next(error);
-    }
+    ApiResponse.send(res, HttpCodes.OK, "Bootcamp retrieved successfully", {
+      bootcamp,
+    });
   }
 
-  async createBootcamp(req: Request, res: Response, next: NextFunction) {
-    try {
-      req.body.instructor = req.user?._id;
+  async createBootcamp(req: Request, res: Response) {
+    req.body.instructor = req.user?._id;
 
-      const newBootcamp = await bootcampService.createBootcamp(req.body);
+    const newBootcamp = await bootcampService.createBootcamp(req.body);
 
-      res.status(201).json({
-        status: "success",
-        data: {
-          bootcamp: newBootcamp,
-        },
-      });
-    } catch (error) {
-      next(error);
-    }
+    ApiResponse.send(res, HttpCodes.CREATED, "Bootcamp created successfully", {
+      bootcamp: newBootcamp,
+    });
   }
-  async deleteBootcamp(req: Request, res: Response, next: NextFunction) {
-    try {
-      const bootcampId = req.params.id as string;
-      const userId = req.user!._id.toString();
-      const userRole = req.user!.role;
 
-      await bootcampService.deleteBootcamp(bootcampId, userId, userRole);
+  async deleteBootcamp(req: Request, res: Response) {
+    const bootcampId = req.params.id as string;
+    const userId = req.user!._id.toString();
+    const userRole = req.user!.role;
 
-      res.status(204).json({
-        status: "success",
-        data: null,
-      });
-    } catch (error) {
-      next(error);
-    }
+    await bootcampService.deleteBootcamp(bootcampId, userId, userRole);
+
+    ApiResponse.send(
+      res,
+      HttpCodes.NO_CONTENT,
+      "Bootcamp deleted successfully",
+    );
   }
-  async updateBootcamp(req: Request, res: Response, next: NextFunction) {
-    try {
-      const bootcampId = req.params.id as string;
-      const updatedData = req.body;
-      const userId = req.user!._id.toString();
-      const userRole = req.user!.role;
 
-      const updatedBootcamp = await bootcampService.updateBootcamp(
-        bootcampId,
-        updatedData,
-        userId,
-        userRole,
+  async updateBootcamp(req: Request, res: Response) {
+    const bootcampId = req.params.id as string;
+    const updatedData = req.body;
+    const userId = req.user!._id.toString();
+    const userRole = req.user!.role;
+
+    const updatedBootcamp = await bootcampService.updateBootcamp(
+      bootcampId,
+      updatedData,
+      userId,
+      userRole,
+    );
+
+    ApiResponse.send(res, HttpCodes.OK, "Bootcamp updated successfully", {
+      bootcamp: updatedBootcamp,
+    });
+  }
+
+  async uploadBootcampPhoto(req: Request, res: Response) {
+    if (!req.file) {
+      AppError.throwError(
+        "BootcampController.uploadBootcampPhoto",
+        HttpCodes.BAD_REQUEST,
+        AppCodes.INVALID_INPUT,
+        "Please upload a file",
       );
-
-      res.status(200).json({
-        status: "success",
-        data: {
-          bootcamp: updatedBootcamp,
-        },
-      });
-    } catch (error) {
-      next(error);
     }
-  }
-  async uploadBootcampPhoto(req: Request, res: Response, next: NextFunction) {
-    try {
-      if (!req.file) {
-        throw new AppError("Please upload a file", 400);
-      }
-      const bootcampId = req.params.id as string;
-      const filename = req.file.filename;
-      const userId = req.user!._id.toString();
-      const userRole = req.user!.role;
 
-      const updatedBootcamp = await bootcampService.uploadBootcampImage(
-        bootcampId,
-        filename,
-        userId,
-        userRole,
-      );
-      res.status(200).json({
-        status: "success",
-        data: {
-          photo: filename,
-          bootcamp: updatedBootcamp,
-        },
-      });
-    } catch (error) {
-      next(error);
-    }
+    const bootcampId = req.params.id as string;
+    const filename = req.file.filename;
+    const userId = req.user!._id.toString();
+    const userRole = req.user!.role;
+
+    const updatedBootcamp = await bootcampService.uploadBootcampImage(
+      bootcampId,
+      filename,
+      userId,
+      userRole,
+    );
+
+    ApiResponse.send(
+      res,
+      HttpCodes.OK,
+      "Bootcamp photo uploaded successfully",
+      {
+        photo: filename,
+        bootcamp: updatedBootcamp,
+      },
+    );
   }
 }
 
