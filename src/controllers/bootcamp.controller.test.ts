e39585ef17e61ee9383f6c33bcc23c1e93a62282
AMIRALI <alimoradi7718@gmail.com>
2026-08-11 13:08:10 +0330

    test: add initial test structure for BootcampController with getAllBootcamps placeholder

---
+++ src/controllers/bootcamp.controller.test.ts
@@ -0,0 +1,13 @@
+import { beforeEach, describe, vi } from "vitest";
+
+describe("BootcampController", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  describe("getAllBootcamps", async () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+});
